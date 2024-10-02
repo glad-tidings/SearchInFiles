b9a9db50b2be42b3b3c55c4926bd9ed6f455ea54
Glad Tidings <37252168+glad-tidings@users.noreply.github.com>
2024-10-02 07:38:14 +0330

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.lvDir = New System.Windows.Forms.ListView()
+        Me.Index = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.FileName = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Size = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Window
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(573, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(591, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Folder"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Location = New System.Drawing.Point(591, 35)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Search"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.Window
+        Me.TextBox2.Location = New System.Drawing.Point(81, 38)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(504, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"*.*", "*.php", "*.txt", "*.asp", "*.htm", "*.html", "*.aspx", "*.asax", "*.xml"})
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 37)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(63, 21)
+        Me.ComboBox1.TabIndex = 5
+        '
+        'lvDir
+        '
+        Me.lvDir.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lvDir.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.Index, Me.FileName, Me.Size})
+        Me.lvDir.FullRowSelect = True
+        Me.lvDir.GridLines = True
+        Me.lvDir.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable
+        Me.lvDir.Location = New System.Drawing.Point(12, 64)
+        Me.lvDir.MultiSelect = False
+        Me.lvDir.Name = "lvDir"
+        Me.lvDir.Size = New System.Drawing.Size(654, 311)
+        Me.lvDir.TabIndex = 6
+        Me.lvDir.UseCompatibleStateImageBehavior = False
+        Me.lvDir.View = System.Windows.Forms.View.Details
+        '
+        'Index
+        '
+        Me.Index.Text = "Index"
+        Me.Index.Width = 51
+        '
+        'FileName
+        '
+        Me.FileName.Text = "FileName"
+        Me.FileName.Width = 526
+        '
+        'Size
+        '
+        Me.Size.Text = "Size"
+        Me.Size.Width = 51
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 378)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(678, 22)
+        Me.StatusStrip1.TabIndex = 7
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(0, 17)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(678, 400)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.lvDir)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents lvDir As System.Windows.Forms.ListView
+    Friend WithEvents Index As System.Windows.Forms.ColumnHeader
+    Friend WithEvents FileName As System.Windows.Forms.ColumnHeader
+    Friend WithEvents Size As System.Windows.Forms.ColumnHeader
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,63 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Dim intNum As Integer = 0
+    Dim s1, s2 As String
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        FolderBrowserDialog1.ShowDialog()
+        TextBox1.Text = FolderBrowserDialog1.SelectedPath
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        intNum = 0
+        lvDir.Items.Clear()
+        Button2.Enabled = False
+        DisplayFile(TextBox1.Text)
+        Button2.Enabled = True
+        ToolStripStatusLabel1.Text = lvDir.Items.Count & " File Found"
+    End Sub
+
+    Private Sub DisplayFile(ByVal strDir As String)
+        Dim reader As System.IO.StreamReader
+        Dim df As DirectoryInfo = New DirectoryInfo(strDir)
+        Dim fi As FileInfo() = df.GetFiles(ComboBox1.Text)
+        Dim lvi As ListViewItem
+        For i As Integer = 0 To (fi.GetLength(0) - 1)
+            s1 = "" : s2 = ""
+            s1 = fi(i).FullName
+            ToolStripStatusLabel1.Text = s1
+            reader = New System.IO.StreamReader(s1)
+            s2 = reader.ReadToEnd
+            If InStr(s2, TextBox2.Text) <> 0 Then
+                intNum += 1
+                lvi = lvDir.Items.Add(intNum.ToString())
+                lvi.SubItems.Add(s1)
+                lvi.SubItems.Add((fi(i).Length).ToString())
+            End If
+            reader.Close()
+            Application.DoEvents()
+        Next
+
+        Dim di() As DirectoryInfo = df.GetDirectories
+        For j As Integer = 0 To (di.GetLength(0) - 1)
+            DisplayFile(di(j).FullName)
+        Next
+
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        ComboBox1.SelectedIndex = 0
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As System.EventArgs) Handles Me.Resize
+        FileName.Width = Me.Width - 168
+    End Sub
+
+    Private Sub lvDir_DoubleClick(sender As Object, e As System.EventArgs) Handles lvDir.DoubleClick
+        If lvDir.SelectedItems.Count <> 0 Then
+            Shell("notepad.exe " & lvDir.SelectedItems(0).SubItems(1).Text, AppWinStyle.NormalFocus)
+        End If
+    End Sub
+End Class
